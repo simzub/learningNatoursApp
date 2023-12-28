6814133a96122ab91505c26fb35145b78c4d09cc
simzub <78310955+simzub@users.noreply.github.com>
2023-12-28 11:42:52 +0200

    handling errors in uncaught exceptions and unhandled rejections and add smarter prod errors

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -20,7 +25,13 @@ mongoose
   .then(() => console.log('DB connection succesful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+process.on('unhandledRejection', (err) => {
+  console.log(err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
