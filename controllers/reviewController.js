31b1473824b3e92d12b1929a6831030365d0d958
simzub <78310955+simzub@users.noreply.github.com>
2024-01-15 11:43:53 +0200

    add nested routes and add factory handler functions for deleting

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,8 +1,11 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
 
   res
     .status(200)
@@ -10,7 +13,13 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({ status: 'success', data: { review: newReview } });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
