520c2784cad5161449cb4e9769482e63c0f86e3c
simzub <78310955+simzub@users.noreply.github.com>
2024-01-15 16:36:49 +0200

    add factory for createOne and updateOne

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -12,14 +12,13 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     .json({ status: 'success', results: reviews.length, data: { reviews } });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({ status: 'success', data: { review: newReview } });
-});
-
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
