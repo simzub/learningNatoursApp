9fba24bb5ed2166cfed21e177addc731f1027e9f
simzub <78310955+simzub@users.noreply.github.com>
2024-01-23 21:08:14 +0200

    add tour page controller, route

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,8 +14,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) get the data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  // 2) no need
+
+  // 3) render template using the data from 1)
+  console.log(tour);
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
